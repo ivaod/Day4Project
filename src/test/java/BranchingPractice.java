563718dfa9bd32ce8d11e9b8949097ff9d9dd62a
ivaod <iva_orlinova@yahoo.com>
2021-05-25 19:23:46 -0400

    shared on github

--- src/test/java/BranchingPractice.java
+++ src/test/java/BranchingPractice.java
@@ -1,5 +1,10 @@
 public class BranchingPractice {
     public static void main(String[] args) {
+        //created new project
+        //enable a version control
+        //added .gitignore with .idea, target folder and iml file
+        //made initial commit by keep track of all files
+        //shared projects on gitHub
 
     }
 }
